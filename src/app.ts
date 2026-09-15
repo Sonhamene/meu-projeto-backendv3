21e62666b3b9018943599dc8a6cc7bd7766211d3
I1HSC <sonhamne3@gmail.com>
2026-09-15 13:21:34 -0300

    correção de erros em rotas POST

--- src/app.ts
+++ src/app.ts
@@ -45,7 +45,7 @@ app.post("/player/attack", (req: Request, res: Response) => {
 
 // Rota para receber o jogador recebr dano
 // Quando o usuário acessar a rota "/player/damage", o servidor chamará o método takeDamage() do jogador, passando o valor do dano recebido como parâmetro
-app.post("/player/take-damage", (req: Request, res: Response) => {
+app.post("/player/takedamage", (req: Request, res: Response) => {
   const { damage } = req.body; // Extrai o valor de dano da requisição
   const damageMessage = player1.takeDamage(damage); // chama o metodo takeDamge() do jogador
   // Retorna uma resposta JSON com a mensagem de dano para o cliente que fez a requisição
@@ -63,5 +63,5 @@ app.listen(PORT, () => {
   console.log("Rotas disponíveis:")
   console.log(`GET http://localhost:${PORT}/player - Obter informações do jogador`);
   console.log(`POST http://localhost:${PORT}/player/attack - Jogador realiza um ataque`);
-  console.log(`POST http://localhost:${PORT}/player/damage - Jogador recebe dano`);
+  console.log(`POST http://localhost:${PORT}/player/takedamage - Jogador recebe dano`);
 });
